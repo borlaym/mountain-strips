339306f1f92900ea4c504505aadd4eb98d8ec19b
Marton Borlay <borlaym@gmail.com>
2019-02-24 07:52:43 +0100

    chore(): build

---
+++ docs/precache-manifest.da667486bbd5c76eba859126757662bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1aae9ca28e2603651f41",
+    "url": "/static/js/main.1aae9ca2.chunk.js"
+  },
+  {
+    "revision": "6ea0f04ba8818b73babe",
+    "url": "/static/js/2.6ea0f04b.chunk.js"
+  },
+  {
+    "revision": "1aae9ca28e2603651f41",
+    "url": "/static/css/main.89273a61.chunk.css"
+  },
+  {
+    "revision": "ee9775cf1a66e668195ea907af7f1e70",
+    "url": "/index.html"
+  }
+];
